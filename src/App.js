0f27914fe209385db87a4cd430d724b50cdf6c07
Harsh8279 <hhenu8279@gmail.com>
2023-01-31 12:46:12 +0530

    change again to original name

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-// import Greet from './components/Greet';
-import Mycomponent from './components/Greet';
+import Greet from './components/Greet';
+// import Mycomponent from './components/Greet';
 
 function App() {
   return (
     <div className="App">
-      <Mycomponent />
+      {/* <Mycomponent /> */}
+      <Greet />
     </div>
   );
 }
