c65bde00860e01e1292fb788615a7606b9030794
Harsh8279 <hhenu8279@gmail.com>
2023-01-31 12:42:49 +0530

    changed normal function to arrow function

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
-function Greet() {
-  return (
-    <div>
-        <h1>Hello Harsh</h1>
-    </div>
-  )
-}
+// function Greet() {
+//   return (
+//     <div>
+//         <h1>Hello Harsh</h1>
+//     </div>
+//   )
+// }
+
+const Greet = () => <h1>Hello Harsh!!</h1>
 
 export default Greet
