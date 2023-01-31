ff1c13c06ac5811398def6b72549214e10d0e722
Harsh8279 <hhenu8279@gmail.com>
2023-01-31 14:02:48 +0530

    Class component concept

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
+import Welcome from './components/Welcome';
 // import Mycomponent from './components/Greet';
 
 function App() {
   return (
     <div className="App">
       {/* <Mycomponent /> */}
-      <Greet />
+      {/* <Greet /> */}
+      <Welcome />
     </div>
   );
 }
