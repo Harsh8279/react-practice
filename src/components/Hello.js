29ac6ba479bcaf2f65f214f650db8aff0b4c9077
Harsh8279 <hhenu8279@gmail.com>
2023-01-31 14:21:34 +0530

    without jsx and h1 tag

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,11 +1,14 @@
 import React from "react"
 
 const Hello = () => {
-    return(
-        <div>
-            Hello Harsh !!!
-        </div>
-    )
+    // return(
+    //     <div>
+    //         Hello Harsh !!!
+    //     </div>
+    // )
+
+    return React.createElement('div',null,"Hello Harsh");
+
 }
 
 export default Hello;
