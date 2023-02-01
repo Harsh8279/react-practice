8071c0c025f7dc72781f8c98b7c2258268ffccca
Harsh8279 <hhenu8279@gmail.com>
2023-02-01 10:26:48 +0530

    tried to change directly message in changeMessage method

--- src/components/Message.js
+++ src/components/Message.js
@@ -10,11 +10,15 @@ class Message extends Component {
         }
     }
 
+    changeMessage(){
+        this.state = "Thnk you for subscribing :)"
+    }
+
     render() {
         return (
             <div>
                 <h1>{this.state.message}</h1>
-                <button> Subscribe </button>
+                <button onClick={()=>this.changeMessage()}> Subscribe </button>
             </div>
         );
     }
