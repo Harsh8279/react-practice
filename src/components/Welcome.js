267941d67d5d06cc1775e0c9cb1abaaf6f64089c
Harsh8279 <hhenu8279@gmail.com>
2023-02-02 13:53:54 +0530

    concept of destructuring state in class component

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,6 +5,7 @@ export class Welcome extends Component {
   render() {
 
     const {name,heroName} = this.props    // destructuring props in class component but in render method
+    const {state1,state2} = this.state
 
     return (
       <div>
