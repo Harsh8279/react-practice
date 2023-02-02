ca74b5a4ac354f467eaee0c5618546f2177b382b
Harsh8279 <hhenu8279@gmail.com>
2023-02-02 23:55:00 +0530

    bind this in render method

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -11,12 +11,13 @@ export class Subscribe extends Component {
         // this.clickHandler = this.clickHandler.bind(this);   // 1st way
     }
 
-    clickHandler = ()=>{  
+    clickHandler (){  
         console.log(this);      // now this has value because we defined Handler with the use of  arrow function
         // here this keyword has no value still clickHandler is class method
         // to solve this we have two ways 
         // 1. bind method 
         // 2. use arrowfunction
+        // 3. bind this in render method
         this.setState(
         {message:"Thank you bruh!!"}
         )
@@ -26,7 +27,7 @@ export class Subscribe extends Component {
         return (
             <div>
                 <h1>{this.state.message}</h1>
-                <button onClick={this.clickHandler}>Subscribe</button>
+                <button onClick={this.clickHandler.bind(this)}>Subscribe</button>   // 3rd way
             </div>
         )
     }
