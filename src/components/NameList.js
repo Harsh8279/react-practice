1f8fa1916781ed77a14fb493af9fae89a51926de
Harsh8279 <hhenu8279@gmail.com>
2023-02-03 12:26:02 +0530

    refactor map in seperate variable and then send via JSX

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,14 +1,9 @@
 import React from 'react'
 
 function NameList() {
-    const names = ["Harsh","Swayam","Hetarth"]
-  return (
-    <div>
-      {
-        names.map(name => <h2>{name} from Surat</h2>)
-      }
-    </div>
-  )
+    const names = ["Harsh", "Swayam", "Hetarth"]
+    const nameList = names.map(name => <h2>{name} from Surat</h2>)
+    return <div>{nameList}</div>
 }
 
 export default NameList
