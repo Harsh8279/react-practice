2a620f9826fb002b579210bb5eb521e8230b4495
Harsh8279 <hhenu8279@gmail.com>
2023-02-03 12:42:26 +0530

    added logic for objectlists

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,8 +1,29 @@
 import React from 'react'
 
 function NameList() {
-    const names = ["Harsh", "Swayam", "Hetarth"]
-    const nameList = names.map(name => <h2>{name} from Surat</h2>)
+    const persons = [
+        {
+            id: 1,
+            name: "Harsh",
+            age: 22,
+            skill: "React"
+        },
+        {
+            id: 2,
+            name: "Swayam",
+            age: 17,
+            skill: "Gaming"
+        },
+        {
+            id: 3,
+            name: "Hetarth",
+            age: 6,
+            skill: "Commnuication"
+        },
+    ]
+
+    const nameList = persons.map(person => <h2>I am {person.name}. I am {person.age} years old. I am skilled in {person.skill}</h2>)
+
     return <div>{nameList}</div>
 }
 
