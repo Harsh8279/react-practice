a31bd6f0e79caf575bc3b6f215f61cacb5a1f626
Harsh8279 <hhenu8279@gmail.com>
2023-02-03 12:13:04 +0530

    rendering list using map and arrow function

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -4,9 +4,9 @@ function NameList() {
     const names = ["Harsh","Swayam","Hetarth"]
   return (
     <div>
-      <h2>{names[0]}</h2>
-      <h2>{names[1]}</h2>
-      <h2>{names[2]}</h2>
+      {
+        names.map(name => <h2>{name} from Surat</h2>)
+      }
     </div>
   )
 }
