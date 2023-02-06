7f49b62f9a97d3f43259c4cb7822c06914d919bd
Harsh8279 <hhenu8279@gmail.com>
2023-02-06 14:10:56 +0530

    index value as a key for list rendering

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Person from './Person'
 
 function NameList() {
+    const names = ["Harsh","Swayam","Hetarth", "Harsh"]
     const persons = [
         {
             id: 1,
@@ -23,9 +24,11 @@ function NameList() {
         },
     ]
 
-    const nameList = persons.map(person => 
-        <Person key={person.id} person={person} />
-        )
+    const nameList = names.map((name,index)=><h2 key={index}>{index}{name}</h2>)
+
+    // const nameList = persons.map(person => 
+    //     <Person key={person.id} person={person} />
+    //     )
         // key props should be unique within the list
         // and must include while creating list of elements
         // key props are not accessible in child components
